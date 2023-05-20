ad330fd273f1429941e4594a92da440fcf334320
alperenyildiz <alperenyildiz@sabanciuniv.edu>
2023-05-20 21:37:39 +0300

    read file

--- phase1_template.asm
+++ phase1_template.asm
@@ -3,7 +3,7 @@ T0: .space 4                           # the pointers to your lookup tables
 T1: .space 4                           
 T2: .space 4                           
 T3: .space 4                           
-fin: .asciiz "C:\\codes\\AES.dat"      # put the fullpath name of the file AES.dat here
+fin: .asciiz "/Users/alperenyildiz/CS401-Project/tables.dat"      # put the fullpath name of the file AES.dat here
 buffer: .space 5000                    # temporary buffer to read from file
 
 .text
@@ -19,7 +19,7 @@ move $s6, $v0      # save the file descriptor
 li   $v0, 14       # system call for read from file
 move $a0, $s6      # file descriptor 
 la   $a1, buffer   # address of buffer to which to read
-li   $a2, 4096     # hardcoded buffer length
+li   $a2, 3070     # hardcoded buffer length
 syscall            # read from file
 
 move $s0, $v0	   # the number of characters read from the file
@@ -27,6 +27,66 @@ la   $s1, buffer   # address of buffer that keeps the characters
 
 
 # your code goes here
+
+
+li $v0, 4       # System call code for printing a string
+move $a0, $a1   # Pass the address of the buffer in $a0
+syscall  
+
+######read again
+li   $v0, 14       # system call for read from file
+move $a0, $s6      # file descriptor 
+la   $a1, buffer   # address of buffer to which to read
+li   $a2, 3070     # hardcoded buffer length
+syscall            # read from file
+
+move $s0, $v0	   # the number of characters read from the file
+la   $s1, buffer   # address of buffer that keeps the characters
+
+
+li $v0, 4       # System call code for printing a string
+move $a0, $a1   # Pass the address of the buffer in $a0
+syscall  
+
+
+######read again
+######read again
+li   $v0, 14       # system call for read from file
+move $a0, $s6      # file descriptor 
+la   $a1, buffer   # address of buffer to which to read
+li   $a2, 3070     # hardcoded buffer length
+syscall            # read from file
+
+move $s0, $v0	   # the number of characters read from the file
+la   $s1, buffer   # address of buffer that keeps the characters
+
+
+li $v0, 4       # System call code for printing a string
+move $a0, $a1   # Pass the address of the buffer in $a0
+syscall  
+
+
+######read again
+######read again
+li   $v0, 14       # system call for read from file
+move $a0, $s6      # file descriptor 
+la   $a1, buffer   # address of buffer to which to read
+li   $a2, 3070     # hardcoded buffer length
+syscall            # read from file
+
+move $s0, $v0	   # the number of characters read from the file
+la   $s1, buffer   # address of buffer that keeps the characters
+
+
+li $v0, 4       # System call code for printing a string
+move $a0, $a1   # Pass the address of the buffer in $a0
+syscall  
+
+
+######read again
+
+
+
 
 Exit:
 li $v0,10
