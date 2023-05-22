756d5ac9331f9d703b11633212e722e304f1395b
alperenyildiz <alperenyildiz@sabanciuniv.edu>
2023-05-22 11:34:55 +0300

    added char to int

--- phase1_template.asm
+++ phase1_template.asm
@@ -3,15 +3,14 @@ T0: .space 4                           # the pointers to your lookup tables
 T1: .space 4                           
 T2: .space 4                           
 T3: .space 4                           
-Hexa: .word "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"
 fin: .asciiz "/Users/alperenyildiz/CS401-Project/tables.dat"      # put the fullpath name of the file AES.dat here
 buffer: .space 5000                    # temporary buffer to read from file
 
-
+test_data: .asciiz "5"
 temp: .space 5000
 
-char_zero: .asciiz "0"
-char_a: .asciiz "a"
+char_zero: .ascii "0"
+char_a: .ascii "a"
 
 .text
 #open a file for writing
@@ -30,95 +29,64 @@ li   $a2, 1     # hardcoded buffer length
 syscall            # read from file
 
 move $s0, $v0	   # the number of characters read from the file
-la   $s1, buffer   # address of buffer that keeps the characters
+la   $s1, buffer   # 
+
+la $a0, test_data
+
+addiu $t1, $a0, 5    # Get the address of the 6th character (index 5)
+lb $t2, 0($t1)       # Load the ASCII value into $t2
+    
+
+jal CHAR_TO_NUM
 
 
-# your code goes here
-
-CONCAT:
-	#a0 is the first string
-	#a1 is the second string
-	#a2 is the buffer string
-
-HEXA_TO_BINARY:
-
-	#a0 is input which is a string of a hexadecimal number
-
-	### start function ###
-	sub $sp, $sp, 8
-	sw  $ra, 4($sp)
-	sw  $a0, 0($sp)
-	### start function ###
-	
-	la $t3, char_zero       # Load the address of the character "0" into $t0
-        lb $s1, ($t3)      # Load the ASCII value of the character into $t1
+j Exit
 
 
-	la $t4, char_a       # Load the address of the character "0" into $t0
-        lb $s2, ($t4)      # Load the ASCII value of the character into $t1
 
 
-	
-	### start loop ###
-	move $t0, $zero
-	Loop1: slti $t1, $t0, 9
-	beq $t1, $zero, Exit1 
-	### start loop ###
-	
-	
-		#$t2 = input[i]
-		sll $t2, $t0, 2 #4i
-		add $t2, $t2, $a0 #base + 4i
-		lw $t2, 0($t2) #t2 = array[i]
-		#$t2 = input[i]
-		
-		sub $t3, $t2, $s1 #if the character is 0-9 then this will be its integer value if not, it will be greater than 9
-		
-		### start if ### 
-		slti $t4, $t3, 10
-		bne $t4, $zero , EXIT_IF_1
-		### start if ###
-		
-			sub $t3, $t2, $s2 #if the character is 0-9 then this will be its integer value if not, it will be greater than 9
-			addi $t3, $t3, 10
-			
-		### end if ###
-		EXIT_IF_1:
-		### end if ###
-		
-		
-		### get ith element of Hexa look up ###
-		la $s3, Hexa
-		sll $t3, $t3, 2
-		addi $t3, $s3, $t3
-		### get ith element of Hexa look up ###
-		
-		
-		la $t4, temp
-		add $a0, $t4, $zero
-		add $a1, $t3, $zero
-		add $a2, $t4, $zero
-		
-		jal CONCAT
-		
-		lw $a0, 0($sp)
-		lw $ra, 8($sp)
-			
-	
-	### end loop ###
-	Exit1:
-	### end loop ###
-	
+PRINT:
+sub $sp, $sp, 4   # we adjust the stack for saving return address and argument
+sw  $ra, 0($sp)   # stores the return address in stack
 
-	
-	
-	
-	### end function ###
-	lw $a0, 0($sp)
-	lw $ra, 8($sp)
-	add $sp, $sp, 8
-	jr $ra
-	### end function ###
+
+li $v0, 4
+syscall 
+
+lw  $ra, 0($sp)   # stores the argument in stack
+addi $sp, $sp, 4
+
+jr $ra
+
+
+PRINT_INT:
+sub $sp, $sp, 4   # we adjust the stack for saving return address and argument
+sw  $ra, 0($sp)   # stores the return address in stack
+
+
+li $v0, 1
+syscall 
+
+lw  $ra, 0($sp)   # stores the argument in stack
+addi $sp, $sp, 4
+
+jr $ra
+
+
+CHAR_TO_NUM:
+sub $sp, $sp, 8   # we adjust the stack for saving return address and argument
+sw  $ra, 4($sp)   # stores the return address in stack
+sw  $a0, 0($sp)   # stores the argument in stack
+
+
+la $t0, test_data
+lb $t2, 0($t0)
+subi $t1, $t2, 48
+
+move $a0, $t1
+jal PRINT_INT
+
+jr $ra
 
 
 Exit:
